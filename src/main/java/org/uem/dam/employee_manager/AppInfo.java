eca16fcb2b170c6b7be6caaf12b2449235c97d7c
alexaib <alexaib@outlook.es>
2022-12-07 13:39:52 +0100

    Added abstract keyword to non-instance class AppInfo
    Signed-off-by: alexaib <alexaib@outlook.es>

--- src/main/java/org/uem/dam/employee_manager/AppInfo.java
+++ src/main/java/org/uem/dam/employee_manager/AppInfo.java
@@ -1,6 +1,6 @@
 package org.uem.dam.employee_manager;
 
-public class AppInfo {
+public abstract class AppInfo {
     public static final String APP_NAME = "Employee Manager";
     public static final String APP_VERSION = "devel";
     public static final int[] APP_SIZE = {800, 600};
