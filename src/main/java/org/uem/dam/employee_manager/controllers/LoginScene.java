33fe5295231e1d2d35284275634f1301500d964e
alexaib <alexaib@outlook.es>
2022-12-08 19:16:14 +0100

    Added elements to scene-login.fxml and bindings to controller
    Signed-off-by: alexaib <alexaib@outlook.es>

--- src/main/java/org/uem/dam/employee_manager/controllers/LoginScene.java
+++ src/main/java/org/uem/dam/employee_manager/controllers/LoginScene.java
@@ -1,8 +1,20 @@
 package org.uem.dam.employee_manager.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginScene extends SceneController {
+    public TextField usernameTextField;
+    public PasswordField passwordTextField;
+
     @Override
     protected void onControllerLoaded() {
 
+    }
+
+
+    public void onLogInAction(ActionEvent actionEvent) {
+        System.err.println("Login action not implemented yet");
     }
 }
